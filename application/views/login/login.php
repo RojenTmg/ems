265a969274e035b2e349c6e0645106431e1135fa
Tseringkhando <candy.khando@gmail.com>
2019-07-04 09:26:40 +0545

    login in controller and view modified... validate login in model modified.

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
-	<?= link_tag('assets/css/login.css'); ?>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+  <title>Login</title>
+  <?= link_tag('assets/css/login.css'); ?>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"  crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous"></script>
@@ -20,23 +20,28 @@
       <div class="main">
          <div class="col-md-6 col-sm-12" style="margin: auto;">
             <div class="login-form">
-               <?php if (!empty(validation_errors())) {
-                echo ' <p class="text-danger">';  
-                       echo 'Fields cannot be empty';
-
-                   echo' </p>';
-            } ?>
+                <?php
+                //invalid credentials message
+                if(isset($_SESSION['error_msg']) && $_SESSION['error_msg']!='')
+                {
+                    echo '<div class="alert alert-danger">'.$_SESSION['error_msg'].' </div>';
+                }
+                ?>
               
-               <form action="loginUser" method="POST">
+               <form action="checkLogin" method="POST">
                   <div class="form-group">
                      <label>Login ID</label>
                      <input type="text" class="form-control" placeholder="User Id" name="user_id">
+                     <!-- error message under the label -->
+                     <span class="text-danger"><?php echo form_error('user_id'); ?></span>
+ 
                   </div>
                   <div class="form-group">
                      <label>Password</label>
                      <input type="password" class="form-control" placeholder="Password" name="password">
+                    <span class="text-danger"><?php echo form_error('password'); ?></span>
                   </div>
-                  <button type="submit" class="btn btn-black">Login</button>
+                  <button type="submit" name="submit" class="btn btn-black">Login</button>
                
                </form>
             </div>
