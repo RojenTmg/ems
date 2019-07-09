19b8ac7054d709c5eda82022b90fb7ac9e3f8798
RojenTmg <35497379+RojenTmg@users.noreply.github.com>
2019-07-09 21:44:42 +0545

    solving bootstrap issue

--- application/views/admin/templates/header.php
+++ application/views/admin/templates/header.php
@@ -16,9 +16,6 @@
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <?= link_tag('assets/css/dashboard.css?version=51') ?>
-    <?= link_tag('assets/css/header.css?version=51') ?>
-    <?= link_tag('assets/css/style.css?version=51') ?>
 
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -28,10 +25,7 @@
     <script type="text/javascript" src="<?= base_url('assets/js/add_employee.js') ?>"></script>
     <script type="text/javascript" src="<?= base_url('assets/js/update_employee.js') ?>"></script>
 
-<<<<<<< HEAD
-<!-- online -->
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
@@ -41,10 +35,9 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-=======
-     
->>>>>>> afc5ab89e220c3a9d4448d48a84216f6ba0979c8
-
+    <?= link_tag('assets/css/dashboard.css?version=51') ?>
+    <?= link_tag('assets/css/header.css?version=51') ?>
+    <?= link_tag('assets/css/style.css?version=51') ?>
 </head>
 
 <body>
