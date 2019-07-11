90ef80a7a1b2f7fdeabf66001043ca2bca51a517
Tseringkhando <candy.khando@gmail.com>
2019-07-11 13:42:58 +0545

    details page

--- application/views/admin/pages/employee_detail.php
+++ application/views/admin/pages/employee_detail.php
@@ -8,8 +8,8 @@
 
 
 			<div class="head-name">
-				<div class="item-1"><?php echo $post['first_name'] . ' ' .  $post['middle_name'] . ' ' .  $post['last_name'];  ?></div>
-				<div class="item-2"><?php echo $post['department_name']; ?></div>
+				<div class="item-1 text-left" ><?php echo $post['first_name'] . ' ' .  $post['middle_name'] . ' ' .  $post['last_name'];  ?></div>
+				<div class="item-2 text-left"><?php echo $post['department_name']; ?></div>
 		</div>
 
 		</div>
@@ -23,43 +23,45 @@
 
 			<div class="card">
 		<h5 class="card-header text-center alert alert-dark">Basic Info</h5>
+		<div class="card-body">
 			<div class="body-row ">
-				<div class="item-1">Id</div>
-				<div class="item-2"><?php echo $post['emp_id']; ?></div>
+
+				<div class="item-1 text-left">Id</div>
+				<div class="item-2 text-left"><?php echo $post['emp_id']; ?></div>
 			</div>
 
 			<div class="body-row ">
-				<div class="item-1">Title</div>
-				<div class="item-2"><?php echo $post['title']; ?></div>
+				<div class="item-1 text-left" >Title</div>
+				<div class="item-2 text-left"><?php echo $post['title']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">First Name</div>
-				<div class="item-2"><?php echo $post['first_name']; ?></div>
+				<div class="item-1 text-left" >First Name</div>
+				<div class="item-2 text-left"><?php echo $post['first_name']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Middle Name</div>
-				<div class="item-2"><?php echo $post['middle_name']; ?></div>
+				<div class="item-1 text-left" >Middle Name</div>
+				<div class="item-2 text-left"><?php echo $post['middle_name']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Surname</div>
-				<div class="item-2"><?php echo $post['last_name']; ?></div>
-			</div>
-
-				<div class="body-row ">
-				<div class="item-1">Gender</div>
-				<div class="item-2"><?php echo $post['gender']; ?></div>
+				<div class="item-1 text-left" >Surname</div>
+				<div class="item-2 text-left"><?php echo $post['last_name']; ?></div>
 			</div>
 
 				<div class="body-row ">
-				<div class="item-1">Date of birth</div>
-				<div class="item-2"><?php echo $post['dob']; ?></div>
+				<div class="item-1 text-left" >Gender</div>
+				<div class="item-2 text-left"><?php echo $post['gender']; ?></div>
+			</div>
+
+				<div class="body-row ">
+				<div class="item-1 text-left" >Date of birth</div>
+				<div class="item-2 text-left"><?php echo $post['dob']; ?></div>
 			</div>
 
 
 
 			<div class="body-row ">
-				<div class="item-1">Permanent Address</div>
-				<div class="item-2"><?php if (!empty($post['p_street'])) echo $post['p_street'] . ', '; 
+				<div class="item-1 text-left" >Permanent Address</div>
+				<div class="item-2 text-left"><?php if (!empty($post['p_street'])) echo $post['p_street'] . ', '; 
 									      if (!empty($post['p_municipality'])) echo $post['p_municipality'] . ', ';
 									      if (!empty($post['p_district'])) echo $post['p_district'] . ', ';
 									      if (!empty($post['p_state'])) echo $post['p_state'] . ', ';
@@ -67,8 +69,8 @@
 			</div>
 
 			<div class="body-row ">
-				<div class="item-1">Temporary Address</div>
-				<div class="item-2"><?php if (!empty($post['t_street'])) echo $post['t_street'] . ', '; 
+				<div class="item-1 text-left" >Temporary Address</div>
+				<div class="item-2 text-left"><?php if (!empty($post['t_street'])) echo $post['t_street'] . ', '; 
 									      if (!empty($post['t_municipality'])) echo $post['t_municipality'] . ', ';
 									      if (!empty($post['t_district'])) echo $post['t_district'] . ', ';
 									      if (!empty($post['t_state'])) echo $post['t_state'] . ', ';
@@ -76,25 +78,27 @@
 			</div>
 
 			<div class="body-row ">
-				<div class="item-1">Home Phone</div>
-				<div class="item-2"><?php echo $post['home_phone']; ?></div>
+				<div class="item-1 text-left" >Home Phone</div>
+				<div class="item-2 text-left"><?php echo $post['home_phone']; ?></div>
 			</div>
 
 			<div class="body-row ">
-				<div class="item-1">Mobile Phone</div>
-				<div class="item-2"><?php echo $post['mobile_phone']; ?></div>
+				<div class="item-1 text-left" >Mobile Phone</div>
+				<div class="item-2 text-left"><?php echo $post['mobile_phone']; ?></div>
 			</div>
 
 			<div class="body-row ">
-				<div class="item-1">Other Phones</div>
-				<div class="item-2"><?php if (!empty($post['other_phone1'])) echo $post['other_phone1'] . ', '; 
+				<div class="item-1 text-left" >Other Phones</div>
+				<div class="item-2 text-left"><?php if (!empty($post['other_phone1'])) echo $post['other_phone1'] . ', '; 
 									      if (!empty($post['other_phone2'])) echo $post['other_phone2'] . ', ';
 									      if (!empty($post['other_phone3'])) echo $post['other_phone3'] ; ?></div>
 			</div>
 
 				<div class="body-row ">
-				<div class="item-1">Email Id</div>
-				<div class="item-2"><?php echo $post['email']; ?></div>
+				<div class="item-1 text-left" >Email Id</div>
+				<div class="item-2 text-left"><?php echo $post['email']; ?></div>
+			</div>
+
 			</div>
 		</div>
 	</div>
@@ -103,22 +107,24 @@
 		<div class="column ">
 			<div class="card">
 			<h5 class="card-header text-center alert alert-dark">Education</h5>	
+			<div class="card-body">
 			<div class="body-row ">
-				<div class="item-1">Highest Degree</div>
-				<div class="item-2"><?php echo $post['highest_degree']; ?></div>
+				<div class="item-1 text-left" >Highest Degree</div>
+				<div class="item-2 text-left"><?php echo $post['highest_degree']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Degree Title</div>
-				<div class="item-2"><?php echo $post['degree_title']; ?></div>
+				<div class="item-1 text-left" >Degree Title</div>
+				<div class="item-2 text-left"><?php echo $post['degree_title']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">University</div>
-				<div class="item-2"><?php echo $post['university']; ?></div>
+				<div class="item-1 text-left" >University</div>
+				<div class="item-2 text-left"><?php echo $post['university']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Institute</div>
-				<div class="item-2"><?php echo $post['institute']; ?></div>
+				<div class="item-1 text-left" >Institute</div>
+				<div class="item-2 text-left"><?php echo $post['institute']; ?></div>
 			</div>
+		</div>
 		</div>
 	</div>
 
@@ -134,30 +140,32 @@
 			<div class="column">
 			<div class="card">
 				<h5 class="card-header text-center alert alert-dark">Nationality</h5>
+				<div class="card-body">
 			<div class="body-row ">
-				<div class="item-1">Nationality</div>
-				<div class="item-2"><?php echo $post['nationality']; ?></div>
+				<div class="item-1 text-left" >Nationality</div>
+				<div class="item-2 text-left"><?php echo $post['nationality']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Visa Permission</div>
-				<div class="item-2"><?php echo $post['visa_permission']; ?></div>
+				<div class="item-1 text-left" >Visa Permission</div>
+				<div class="item-2 text-left"><?php echo $post['visa_permission']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Visa Type</div>
-				<div class="item-2"><?php echo $post['visa_type']; ?></div>
+				<div class="item-1 text-left" >Visa Type</div>
+				<div class="item-2 text-left"><?php echo $post['visa_type']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Visa Expiry Date</div>
-				<div class="item-2"><?php echo $post['visa_expiry_date']; ?></div>
+				<div class="item-1 text-left" >Visa Expiry Date</div>
+				<div class="item-2 text-left"><?php echo $post['visa_expiry_date']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Passport No.</div>
-				<div class="item-2"><?php echo $post['passport_no']; ?></div>
+				<div class="item-1 text-left" >Passport No.</div>
+				<div class="item-2 text-left"><?php echo $post['passport_no']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Place of Issue</div>
-				<div class="item-2"><?php echo $post['passport_issue_place']; ?></div>
+				<div class="item-1 text-left" >Place of Issue</div>
+				<div class="item-2 text-left"><?php echo $post['passport_issue_place']; ?></div>
 			</div>
+		</div>
 
 		</div>
 		</div>
@@ -169,24 +177,26 @@
 		<div class="column ">
 				<div class="card">
 			<h5 class="card-header text-center alert alert-dark">Emergency Contact</h5>
+			<div class="card-body">
 			<div class="body-row ">
-				<div class="item-1">Name</div>
-				<div class="item-2"><?php echo $post['pan']; ?></div>
+				<div class="item-1 text-left" >Name</div>
+				<div class="item-2 text-left"><?php echo $post['pan']; ?></div>
 			</div>
 
 			<div class="body-row ">
-				<div class="item-1">Relation</div>
-				<div class="item-2"><?php echo $post['pan']; ?></div>
+				<div class="item-1 text-left" >Relation</div>
+				<div class="item-2 text-left"><?php echo $post['pan']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Contact</div>
-				<div class="item-2"><?php echo $post['pan']; ?></div>
+				<div class="item-1 text-left" >Contact</div>
+				<div class="item-2 text-left"><?php echo $post['pan']; ?></div>
 			</div>
 
 			<div class="body-row ">
-				<div class="item-1">Address</div>
-				<div class="item-2"><?php echo $post['pan']; ?></div>
+				<div class="item-1 text-left" >Address</div>
+				<div class="item-2 text-left"><?php echo $post['pan']; ?></div>
 			</div>
+		</div>
 		</div>
 </div>	
 
@@ -195,26 +205,28 @@
 <div class="column ">
 				<div class="card">
 	<h5 class="card-header text-center alert alert-dark">Health</h5>
+	<div class="card-body">
 			<div class="body-row ">
-				<div class="item-1">Blood Group</div>
-				<div class="item-2"><?php echo $post['blood_group']; ?></div>
+				<div class="item-1 text-left" >Blood Group</div>
+				<div class="item-2 text-left"><?php echo $post['blood_group']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Medical Complications</div>
-				<div class="item-2"><?php echo $post['medical_complications']; ?></div>
+				<div class="item-1 text-left" >Medical Complications</div>
+				<div class="item-2 text-left"><?php echo $post['medical_complications']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Regular Medication</div>
-				<div class="item-2"><?php echo $post['regular_medication']; ?></div>
+				<div class="item-1 text-left" >Regular Medication</div>
+				<div class="item-2 text-left"><?php echo $post['regular_medication']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Allergies</div>
-				<div class="item-2"><?php echo $post['allergies']; ?></div>
+				<div class="item-1 text-left" >Allergies</div>
+				<div class="item-2 text-left"><?php echo $post['allergies']; ?></div>
 			</div>
 			<div class="body-row ">
-				<div class="item-1">Allergy Description</div>
-				<div class="item-2"><?php echo $post['allergy_description']; ?></div>
+				<div class="item-1 text-left" >Allergy Description</div>
+				<div class="item-2 text-left"><?php echo $post['allergy_description']; ?></div>
 			</div>
+		</div>
 		</div>
 	</div>
 		<!-- health ends here -->
@@ -228,34 +240,36 @@
 			<div class="column">
 				<div class="card">
 				<h5 class="card-header text-center alert alert-dark">Work Experience</h5>
+				<div class="card-body">
 				<div class="body-row ">
-					<div class="item-1">Organization</div>
-					<div class="item-2"><?php echo $post['previous_employer']; ?></div>
+					<div class="item-1 text-left" >Organization</div>
+					<div class="item-2 text-left"><?php echo $post['previous_employer']; ?></div>
 				</div>
 				<div class="body-row ">
-					<div class="item-1">Responsibility</div>
-					<div class="item-2"><?php echo $post['previous_employer']; ?></div>
-				</div>
-
-				<div class="body-row ">
-					<div class="item-1">Contact Person</div>
-					<div class="item-2"><?php echo $post['previous_employer']; ?></div>
+					<div class="item-1 text-left" >Responsibility</div>
+					<div class="item-2 text-left"><?php echo $post['previous_employer']; ?></div>
 				</div>
 
 				<div class="body-row ">
-					<div class="item-1">Contact No.</div>
-					<div class="item-2"><?php echo $post['previous_employer']; ?></div>
+					<div class="item-1 text-left" >Contact Person</div>
+					<div class="item-2 text-left"><?php echo $post['previous_employer']; ?></div>
 				</div>
 
 				<div class="body-row ">
-					<div class="item-1">Contact Address</div>
-					<div class="item-2"><?php echo $post['previous_employer']; ?></div>
+					<div class="item-1 text-left" >Contact No.</div>
+					<div class="item-2 text-left"><?php echo $post['previous_employer']; ?></div>
 				</div>
 
 				<div class="body-row ">
-					<div class="item-1">Time</div>
-					<div class="item-2"><?php echo $post['previous_employer']; ?></div>
+					<div class="item-1 text-left" >Contact Address</div>
+					<div class="item-2 text-left"><?php echo $post['previous_employer']; ?></div>
 				</div>
+
+				<div class="body-row ">
+					<div class="item-1 text-left" >Time</div>
+					<div class="item-2 text-left"><?php echo $post['previous_employer']; ?></div>
+				</div>
+			</div>
 			</div>
 </div>
 		<!-- work ends here  -->
@@ -264,10 +278,12 @@
 		<div class="column ">
 				<div class="card">
 			<h5 class="card-header text-center alert alert-dark">PAN</h5>
+			<div class="card-body">
 			<div class="body-row ">
-				<div class="item-1">PAN</div>
-				<div class="item-2"><?php echo $post['pan']; ?></div>
+				<div class="item-1 text-left" >PAN</div>
+				<div class="item-2 text-left"><?php echo $post['pan']; ?></div>
 			</div>
+		</div>
 		</div>
 	</div>
 		<!-- pan ends here -->
